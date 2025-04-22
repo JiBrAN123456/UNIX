3826a1d041a8663de3a0e5a2e6deb93154060f02
Mohammad Jibran <yunisquadri99@gmail.com>
2025-04-23 02:55:37 +0530

    i think done for now will test and see if anything more is needed

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from .models import Task, User
 
-# Create your tests here.
+class TaskManagerTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.username = "testuser"
+        self.password = "testpass123"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.register_url = reverse('token_obtain_pair')  # JWT login
+        self.task_url = reverse('task-list-create')  # tasks/
+
+        # Login and get token
+        response = self.client.post(self.register_url, {"username": self.username, "password": self.password})
+        self.token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+
+    def test_create_task(self):
+        response = self.client.post(self.task_url, {"name": "Test Task"})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(Task.objects.first().name, "Test Task")
+
+    def test_list_tasks(self):
+        Task.objects.create(user=self.user, name="Sample Task 1")
+        Task.objects.create(user=self.user, name="Sample Task 2")
+
+        response = self.client.get(self.task_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_delete_task(self):
+        task = Task.objects.create(user=self.user, name="Delete Me")
+        delete_url = reverse('task-delete', kwargs={'pk': task.id})
+        response = self.client.delete(delete_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Task.objects.count(), 0)
+
+    def test_filter_task_by_status(self):
+        Task.objects.create(user=self.user, name="Running Task", status='running')
+        Task.objects.create(user=self.user, name="Completed Task", status='completed')
+
+        response = self.client.get(self.task_url + '?status=completed')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['status'], 'completed')
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,13 @@
-# tasks/urls.py
+
 
 from django.urls import path
 from .views import RegisterView, TaskListCreateView, TaskDeleteView, HealthCheckView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', TokenObtainPairView.as_view()),
-    path('refresh/', TokenRefreshView.as_view()),
-    path('tasks/', TaskListCreateView.as_view()),
-    path('tasks/<int:pk>/', TaskDeleteView.as_view()),
-    path('health/', HealthCheckView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('tasks/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
+    path('health/', HealthCheckView.as_view(), name='health-check'),
 ]
